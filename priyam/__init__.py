de7da56e63557778b62db749df4788bae459eb71
shreyazh <shreyash5183@gmail.com>
2025-11-16 18:33:19 +0530

    v2

--- priyam/__init__.py
+++ priyam/__init__.py
@@ -7,3 +7,16 @@ from .physics import *
 from .apps import run_linkchecker as linkchecker
 from .apps import run_notepad as notepad
 from .apps import run_extract as slide_extract
+
+
+__all__ = [
+    "linkchecker",
+    "notepad",
+    "slide_extract",
+    "pmath",
+    "physics",
+    "chemistry",
+    "cs",
+    "string_utils",
+    "latex_support"
+]
