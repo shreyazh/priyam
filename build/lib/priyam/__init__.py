6f259b0383b03798a2f01eb3546eed9578572ea4
shreyazh <shreyash5183@gmail.com>
2025-11-16 16:13:35 +0530

    core file prune

--- build/lib/priyam/__init__.py
+++
@@ -1,9 +0,0 @@
-from .string_utils import *
-from .latex_support import *
-from .pmath import *
-from .chemistry import *
-from .cs import *
-from .physics import *
-from .apps import run_linkchecker as linkchecker
-from .apps import run_notepad as notepad
-from .apps import run_extract as slide_extract
---
+++ test.py
@@ -0,0 +1,2 @@
+from priyam import slide_extract
+slide_extract()
